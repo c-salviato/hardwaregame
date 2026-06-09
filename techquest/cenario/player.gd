e2a960fdd33d5577ba441a86c0ce648e44d07c48
AllanFinoketti <allanfinoketti@gmail.com>
2026-06-08 23:26:13 -0300

    criação do script de movimento do personagem principal
    criei o script para que ele se locomova e adicionei o movimento dos sprites 2 e 3, e quando n há movimento, fica no sprite 1 de sua respectiva linha

---
+++ techquest/cenario/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+const VEL = 150.0
+
+@onready var anim = $Animacao
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("andar_esq", "andar_dir", "andar_cima", "andar_baixo")
+	velocity = direction * VEL
+
+	if direction.x > 0:
+		anim.play("andar_dir")
+	elif direction.x < 0:
+		anim.play("andar_esq")
+	elif direction.y < 0:
+		anim.play("andar_cima")
+	elif direction.y > 0:
+		anim.play("andar_baixo")
+	else:
+		anim.stop()
+
+	move_and_slide()
