5f924d83da87934f9ecd25dbc91e7a2a44c38928
Caique Souza Salviato <caique.s.salviato@gmail.com>
2026-06-28 20:28:47 -0300

    Modificações e configurações
    Arrumei o lobby, atualizei o sprite do portal, arrumei os sprites dos icones do menu do portal, arrumei os dialogos da cena.
    Ajustei o problema do player conseguir andar durante os dialogos ou interações, agora o jogo pausa.
    Adicionei o gabinete e suas funcionalidades principais.
    Modifiquei um pouco as colisões do level01 e arrumei o dialogo inicial do NPC Senhor.
    Alterei algumas configurações de renderização do projeto, pra para de bugar os pixels do player enquanto ele anda.

---
+++ techquest/scripts/Global.gd
@@ -0,0 +1,10 @@
+extends Node
+
+# Variável que diz quantas peças o jogador já pegou nas fases (começa em 0, vai até 6)
+var pecas_coletadas: int = 0
+
+# Variável que diz quantas peças já foram instaladas no gabinete
+var pecas_colocadas: int = 0
+
+# Guarda se a primeira conversa com o pai já foi concluída (começa como falso)
+var dialogo_pai_feito: bool = false
